c99bbb5fedc3cc8b477eadcee7b466d72058ee47
Rigel Lute <rigellute@gmail.com>
2015-11-10 10:54:17 +0000

    Updated Version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
     //    media query for map initialization
-    if (window.matchMedia("(min-width:400px)").matches) {
+    if (window.matchMedia("(min-width: 64.063em)").matches) {
         $("#map-container").storeLocator({
+            'maxDistance': true,
+            'taxonomyFilters': {
+                'features': 'category-filters-container2'
+            },
             "mapSettings": {
                 zoom: 12,
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -11,6 +15,7 @@ $(document).ready(function () {
                 draggable: true
             }
         });
+
 
         console.log('desktop');
     } else {
@@ -27,10 +32,15 @@ $(document).ready(function () {
         console.log('mobile');
     }
 
+    $('.bh-sl-filters-container').hide();
+    $('.filter-service').click(function () {
+        $('.bh-sl-filters-container').toggle(1000);
+    });
+
     //    scroll to method
     var mapPos = $('.bh-sl-map-container').offset().top;
 
-    $('.btn').click(function () {
+    $('#bh-sl-submit').click(function () {
         TweenMax.to(window, 1.5, {
             scrollTo: mapPos,
             delay: 0.2,
@@ -40,14 +50,15 @@ $(document).ready(function () {
 
     //    TweenMax Timeline
     var loadTl = new TimelineMax();
-    loadTl.staggerFrom($('.header-content'), 0.7, {
+    loadTl
+
+        .staggerFrom($('.header-content'), 0.7, {
         x: '-10%',
         y: '20%',
         opacity: 0
-    }, 0.3);
+    }, 0.3)
 
-    // Scroll Magic
-
+    ;
     // init controller
     var controller = new ScrollMagic.Controller({
         globalSceneOptions: {
@@ -56,14 +67,23 @@ $(document).ready(function () {
         }
     });
 
-    // build scenes
-    new ScrollMagic.Scene({
-            triggerElement: "#parallax1"
-        })
-        .setTween(".parallax-1", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        //        .addIndicators()
-        .addTo(controller);
+
+    // Scroll Magic
+    if (window.matchMedia("(min-width:64.063em)").matches) {
+
+
+
+        // build scenes
+        new ScrollMagic.Scene({
+                triggerElement: "#parallax1"
+            })
+            .setTween(".parallax-1", {
+                y: "80%",
+                ease: Linear.easeNone
+            })
+            //        .addIndicators()
+            .addTo(controller);
+    } else {
+        controller.enabled(false);
+    }
 });
