8e938ff6afa4aa4cd382f2d488b19fe9254d4736
Rigel Lute <rigellute@gmail.com>
2016-01-27 14:58:56 +0000

    Massive Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,50 +16,109 @@ $(document).ready(function () {
             }
         });
 
-
+        // Calculate the offset of the results section
+        var resultPos = ($('.search-again-container').offset().top);
+        
+        //Desktop Scroll to the top of the results section when a list item is clicked
+        $(document).on('click', '.list-details', function () {
+            TweenMax.to(window, 1.5, {
+                scrollTo: {
+                    y: $('.search-again-container').offset().top
+                },
+                delay: 0.3,
+                ease: Power2.easeInOut
+            });
+        });
         console.log('desktop');
     } else {
+//        init map
         $("#map-container").storeLocator({
+            'maxDistance': true,
+            'taxonomyFilters': {
+                'features': 'category-filters-container2'
+            },
             "mapSettings": {
                 zoom: 12,
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 disableDoubleClickZoom: false,
-                scrollwheel: false,
+                scrollwheel: true,
                 navigationControl: false,
-                draggable: false
+                draggable: true
             }
         });
-        console.log('mobile');
-    }
+        //MOBILE Scroll to the top of the map when a list item is clicked
+        $(document).on('click', '.list-details', function () {
+            var mapHeightOffset = $('#bh-sl-map').offset().top;
+            TweenMax.to(window, 1.5, {
+                scrollTo: {
+                    y: mapHeightOffset - 1
+                },
+                delay: 0.3,
+                ease: Power2.easeInOut
+            });
+        });
+        // Calculate the height of these element if mobile
+        var parallaxParentHeight = $('.parallaxParent').height();
+        var navbarHeight = $('.navbar').height();
+        var resultPos = (parallaxParentHeight + navbarHeight);
+        
+        $('#map-container').removeClass('container-map');
 
+        console.log('mobile');
+    } // end else
+
+    //    toggle hide filter list
     $('.bh-sl-filters-container').hide();
     $('.filter-service').click(function () {
         $('.bh-sl-filters-container').toggle(1000);
     });
 
-    //    scroll to method
-    var mapPos = $('.bh-sl-map-container').offset().top;
 
+    //    First SCROLL TO METHOD: including some other attempts
+    //    var mapPos = $('#map-container').offset().top;
+    //    var viewPortHeight = $(window).height();
+    var parallaxParentHeight = $('.parallaxParent').height();
+    var navbarHeight = $('.navbar').height();
+    //    var resultPos = (parallaxParentHeight + navbarHeight);
+    //    var resultPos = $('.search-again-container').offset().top;
+
+    //  Init map height variable
+    var mapHeightOffset = 0;
+
+    //Function to Scroll to results and calculate the map offset after it has loaded in
     $('#bh-sl-submit').click(function () {
         TweenMax.to(window, 1.5, {
-            scrollTo: mapPos,
-            delay: 0.2,
+            scrollTo: {
+                y: resultPos
+            },
+            delay: 0.8,
             ease: Power2.easeInOut
         });
+
+        //        Calculate the map offset after it has loaded in
+        setTimeout(function () {
+            mapHeightOffset = $('#bh-sl-map').offset().top;
+        }, 2000);
     });
 
-    //    TweenMax Timeline
+    //    TweenMax Timeline that tweens in the header-content on load
     var loadTl = new TimelineMax();
     loadTl
-
         .staggerFrom($('.header-content'), 0.7, {
-        x: '-10%',
-        y: '20%',
-        opacity: 0
-    }, 0.3)
+            x: '-10%',
+            y: '20%',
+            opacity: 0
+        }, 0.3);
 
-    ;
-    // init controller
+
+    //Make features into list (in the infowindow template)
+    Handlebars.registerHelper('breaklines', function (text) {
+        text = Handlebars.Utils.escapeExpression(text);
+        text = text.replace(/,/g, '<br>');
+        return new Handlebars.SafeString(text);
+    });
+
+    // init parallax controller
     var controller = new ScrollMagic.Controller({
         globalSceneOptions: {
             triggerHook: "onEnter",
@@ -67,11 +126,8 @@ $(document).ready(function () {
         }
     });
 
-
-    // Scroll Magic
+    // Scroll Magic Parallax effect inside media query to disable for mobile
     if (window.matchMedia("(min-width:64.063em)").matches) {
-
-
 
         // build scenes
         new ScrollMagic.Scene({
@@ -81,9 +137,57 @@ $(document).ready(function () {
                 y: "80%",
                 ease: Linear.easeNone
             })
-            //        .addIndicators()
             .addTo(controller);
     } else {
         controller.enabled(false);
     }
+
+
+    //    Scroll magic to Make arrow pin to screen  
+    //    Init the contoller
+    var controller = new ScrollMagic.Controller();
+    //    Create the pin Scene
+    var scenePin = new ScrollMagic.Scene({
+            triggerElement: "#map-container",
+            triggerHook: "onLeave",
+            offset: 40
+        })
+        .setPin("#pin1")
+        .addTo(controller);
+
+    //Tween size of the button
+    var buttonTween = TweenMax.to('#pin1', 1, {fontSize: '1.5em', backgroundColor: '#d2322d', borderColor: 'rgb(212, 63, 58)'});
+    var scenePinScale = new ScrollMagic.Scene({
+            triggerElement: "#map-container",
+            triggerHook: "onLeave",
+            duration: '100%'
+        })
+        .setTween(buttonTween)
+        .addTo(controller);
+
+    //this function triggers when there is a scroll event. It calculates the height of the div and scrolls to it if arrow is clicked 
+    $(window).scroll(function () {
+        if ($(window).scrollTop() < $('.search-again-container').offset().top + 50) {
+            //            console.log($(window).scrollTop());
+            $('#pin1').html('Back to Search');
+            $('#pin1').click(function () {
+                TweenMax.to(window, 1.5, {
+                    scrollTo: {
+                        y: 0
+                    },
+                    ease: Power2.easeInOut
+                });
+            });
+        } else {
+            $('#pin1').html('Back to Results');
+            $('#pin1').click(function () {
+                TweenMax.to(window, 1.5, {
+                    scrollTo: {
+                        y: $('.search-again-container').offset().top
+                    },
+                    ease: Power2.easeInOut
+                });
+            });
+        }
+    }); //end scrolling function
 });
